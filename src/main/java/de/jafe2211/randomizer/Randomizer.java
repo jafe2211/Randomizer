ff1c753202b213ab16396cbb224451b4854a2d3a
Jafe2211 <jakob.fels@web.de>
2025-06-09 22:35:45 +0200

    Initial commit

---
+++ src/main/java/de/jafe2211/randomizer/Randomizer.java
@@ -0,0 +1,17 @@
+package de.jafe2211.randomizer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Randomizer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
